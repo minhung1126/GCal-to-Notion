1f2e0798ef337ba973c4205b639a1a0d88a5b261
Min Hung DeskMeet B660 <minhung1126@gmail.com>
2023-02-04 11:34:51 +0800

    Add logging

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import sys
 from datetime import datetime
+import logging
 
 from utils import gcal
 from utils.notion import Notion
 from utils.history import History
 
 
+def setup_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.handlers.clear()
+
+    history_formatter = logging.Formatter(
+        '[%(asctime)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    file_hdlr = logging.FileHandler(
+        'Worklog.txt', 'a', encoding='utf-8'
+    )
+    file_hdlr.setFormatter(history_formatter)
+    file_hdlr(logging.DEBUG)
+    logger.addHandler(file_hdlr)
+
+    return
+
+
 def main():
+    setup_logging()
     gcal_url, notion_token, notion_db_id = sys.argv[1:]
 
     global history, notion
@@ -34,6 +56,7 @@ def main():
                     'NotionPageID': notion_page_id,
                     'LastModify': event.last_modify,
                 })
+                logging.debug(f'Add: {event.uid} -> {notion_page_id}')
         elif datetime.strptime(event.last_modify, "%Y%m%dT%H%M%SZ") > \
             datetime.strptime(
                 history.search_by_gcal_uid(event.uid).get('LastModify'),
@@ -49,6 +72,7 @@ def main():
                 'GCalUID': event.uid,
                 'LastModify': event.last_modify,
             })
+            logging.debug(f'Modify: {event.uid}')
 
     # Delete events that is deleted on gcal
     gcal_event_uids = set([
@@ -59,6 +83,7 @@ def main():
     for to_delete_uid in uids_in_history-gcal_event_uids:
         notion.delete_by_gcal_uid(to_delete_uid)
         history.delete_by_gcal_uid(to_delete_uid)
+        logging.debug(f'Delete: {to_delete_uid}')
 
     return
 
