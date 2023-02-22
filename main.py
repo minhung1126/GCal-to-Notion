239f52afd5a97bb66cf0fedb7f15035af9b8eb09
Min Hung DeskMeet B660 <minhung1126@gmail.com>
2023-02-22 18:22:59 +0800

    update main.py Test

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ def main():
     except gcal.GCalReadFail:
         write_work_log("Fail to read gcal log")
         return
+    write_work_log(events)
 
     # Add or Modify
     for event in events:
